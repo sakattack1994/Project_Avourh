c2ead8fbc121f105248241e32c20601678891bb0
sakattack1994 <sakis75717293@outlook.com.gr>
2016-12-09 17:23:13 +0200

    wraia domhmeno apsogo me php pleon kai server energo

---
+++ WebPageTemplate.php
@@ -0,0 +1,45 @@
+<html xmlns="http://www.w3.org/1999/xhtml" class="csstransforms no-csstransforms3d csstransitions">
+  <head>
+    <title></title>
+    <link rel="shortcut icon" media="screen,print" href="Images/favicon.png" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="CSS/stylesheets.css">
+    <link rel="stylesheet" type="text/css" href="CSS/menu.css">
+    <link rel="stylesheet" type="text/css" href="CSS/login.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <script type="text/javascript" src="javascript/add_header.js"></script>
+    <script type="text/javascript" src="javascript/add_footer.js"></script>
+    <script type="text/javascript" src="javascript/the_department_scripts.js"></script>
+    <script type="text/javascript" src="javascript/intro_content.js"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+
+  <body background="background.jpg">
+    <div class="container">
+      <div class="row">
+          <div class="col-md-12"><div id="header"></div></div>
+      </div>
+
+      <div class="row">
+          <?php echo $content; ?>
+      </div>
+
+    <div class="row">
+        <div class="col-md-12"><div id="footer"></div></div>
+    </div>
+    </div>
+
+<script>
+add_header();
+login();
+add_footer();
+</script>
+
+    </body>
+</html>
